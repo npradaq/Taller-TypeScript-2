08bf9a310d04e2cca53030c3be3f89144a6a1eec
npradaq <n.pradaq@uniandes.edu.co>
2023-10-22 13:13:06 -0500

    Entrega Taller TypeScript 2

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,22 @@ import { series } from "./Serie.js";
 var seriesTable = document.getElementById("tabla-series");
 mostrarDatosSeries(series);
 mostrarPromedioTemporadas(series);
+actualizarTarjetaSerie(series[0]);
+document.addEventListener("DOMContentLoaded", function () {
+    var filasDeSeries = document.querySelectorAll("#tabla-series tbody tr");
+    filasDeSeries.forEach(function (fila, index) {
+        fila.addEventListener("click", function () {
+            var serieSeleccionada = series[index];
+            actualizarTarjetaSerie(serieSeleccionada);
+        });
+    });
+});
 function mostrarDatosSeries(seriesLocal) {
     var seriesTBody = document.createElement("tbody");
     for (var _i = 0, seriesLocal_1 = seriesLocal; _i < seriesLocal_1.length; _i++) {
         var serie = seriesLocal_1[_i];
         var trElement = document.createElement("tr");
-        trElement.innerHTML = "\n            <td>".concat(serie.id, "</td>\n            <td>").concat(serie.nombre, "</td>\n            <td>").concat(serie.canal, "</td>\n            <td>").concat(serie.temporadas, "</td>\n            ");
+        trElement.innerHTML = "\n            <td>".concat(serie.id, "</td>\n            <td style=\"color: blue; text-decoration: underline; cursor: pointer;\">").concat(serie.nombre, "</td>\n            <td>").concat(serie.canal, "</td>\n            <td>").concat(serie.temporadas, "</td>\n            ");
         seriesTBody.appendChild(trElement);
     }
     seriesTable.appendChild(seriesTBody);
@@ -21,4 +31,14 @@ function mostrarPromedioTemporadas(seriesLocal) {
     }
     var promedio = sum / seriesLocal.length;
     temporadasParrafo.innerHTML = "El promedio de temporadas es: ".concat(promedio);
+}
+function actualizarTarjetaSerie(serie) {
+    var tarjetaNombre = document.querySelector(".card-title");
+    var tarjetaDescripcion = document.querySelector(".card-text");
+    var tarjetaImagen = document.querySelector(".card-img-top");
+    var tarjetaLink = document.querySelector(".btn-primary");
+    tarjetaNombre.textContent = serie.nombre;
+    tarjetaDescripcion.textContent = serie.descripcion;
+    tarjetaImagen.src = serie.imagen;
+    tarjetaLink.href = serie.url;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+
 import { Serie } from "./Serie.js";
 import { series } from "./Serie.js";
 
@@ -5,6 +6,18 @@ let seriesTable: HTMLElement = document.getElementById("tabla-series")!;
 
 mostrarDatosSeries(series);
 mostrarPromedioTemporadas(series);
+actualizarTarjetaSerie(series[0]);
+
+document.addEventListener("DOMContentLoaded", () => {
+    const filasDeSeries: NodeListOf<HTMLTableRowElement> = document.querySelectorAll("#tabla-series tbody tr");
+
+    filasDeSeries.forEach((fila, index) => {
+        fila.addEventListener("click", () => {
+            const serieSeleccionada = series[index];
+            actualizarTarjetaSerie(serieSeleccionada);
+        });
+    });
+});
 
 function mostrarDatosSeries(seriesLocal: Serie[]): void {
     let seriesTBody: HTMLElement = document.createElement("tbody");
@@ -12,7 +25,7 @@ function mostrarDatosSeries(seriesLocal: Serie[]): void {
         let trElement: HTMLElement = document.createElement("tr");
         trElement.innerHTML = `
             <td>${serie.id}</td>
-            <td>${serie.nombre}</td>
+            <td style="color: blue; text-decoration: underline; cursor: pointer;">${serie.nombre}</td>
             <td>${serie.canal}</td>
             <td>${serie.temporadas}</td>
             `;
@@ -33,5 +46,16 @@ function mostrarPromedioTemporadas(seriesLocal: Serie[]): void {
     let promedio: number = sum / seriesLocal.length;
 
     temporadasParrafo.innerHTML = `El promedio de temporadas es: ${promedio}`;
-    
+}
+
+function actualizarTarjetaSerie(serie: Serie) {
+    const tarjetaNombre: HTMLElement = document.querySelector(".card-title")!;
+    const tarjetaDescripcion: HTMLElement = document.querySelector(".card-text")!;
+    const tarjetaImagen: HTMLImageElement = document.querySelector(".card-img-top")!;
+    const tarjetaLink: HTMLAnchorElement = document.querySelector(".btn-primary")!;
+
+    tarjetaNombre.textContent = serie.nombre;
+    tarjetaDescripcion.textContent = serie.descripcion;
+    tarjetaImagen.src = serie.imagen;
+    tarjetaLink.href = serie.url;
 }
